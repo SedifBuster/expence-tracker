c14fff6bdc9c6514fc6e75d6598e9576ee434707
Artem <68074370+SedifBuster@users.noreply.github.com>
2026-04-06 14:08:38 +0300

    fix

--- client/src/features/auth/ui/loginForm.tsx
+++ client/src/features/auth/ui/loginForm.tsx
@@ -78,6 +78,7 @@ export function LoginForm () {
             text-xl
             font-semibold
             text-white
+            
           "
         >For convenience only &#129303;</Legend>
         <Field>
